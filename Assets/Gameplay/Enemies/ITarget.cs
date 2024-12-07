177455ba865b5863af9cb25c1dc464db48a05604
Egor Slesarenko <egor050503sl@gmail.com>
2024-12-07 23:03:39 +0300

    refactore: itarget position

--- Assets/Gameplay/Enemies/ITarget.cs
+++ Assets/Gameplay/Enemies/ITarget.cs
@@ -5,7 +5,7 @@ namespace Gameplay.Enemies
 {
     public interface ITarget : ISceneEntity
     {
-        public Vector3 Position { get; set; }
+        public Vector3 Position { get; }
         public Vector3 Forward { get; }
         public float Speed { get; }
         public void ApplyDamage(int damage);
